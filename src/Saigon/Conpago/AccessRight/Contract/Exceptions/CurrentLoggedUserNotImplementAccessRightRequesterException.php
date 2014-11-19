eb728d8f5145ce665bddf215fa409fe62ac26713
Bartosz Go?ek <bartosz.golek@forcom.com.pl>
2014-11-19 15:08:46 +0100

    composer update CurrentLoggedUserNotImplementAccessRightRequester

---
+++ src/Saigon/Conpago/AccessRight/Contract/Exceptions/CurrentLoggedUserNotImplementAccessRightRequesterException.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bgolek
+	 * Date: 2014-11-19
+	 * Time: 15:07
+	 */
+
+	namespace Saigon\Conpago\AccessRight\Contract\Exceptions;
+
+
+	class CurrentLoggedUserNotImplementAccessRightRequesterException extends \Exception
+	{
+	}
